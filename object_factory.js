b85e36a249ada23dad5a937ecf3a4cc2ca736022
egoing <egoing@gmail.com>
2019-04-07 18:30:21 +0900

    6.3. constructor 만들기

--- object_factory.js
+++ object_factory.js
@@ -1,24 +1,14 @@
-var kim = {
-    name:'kim',
-    first:10,
-    second:20,
-    third:30,
-    sum:function(){
-        return this.first+this.second+this.third;
+function Person(name, first, second, third){
+    this.name=name;
+    this.first=first;
+    this.second=second;
+    this.sum = function(){
+        return this.first+this.second;
     }
 }
-var lee = {
-    name:'lee',
-    first:10,
-    second:10,
-    third:10,
-    sum:function(){
-        return this.first+this.second+this.third;
-    }
-}
+
+var kim = new Person('kim', 10, 20);
+var lee = new Person('lee', 10, 10);
 console.log("kim.sum()", kim.sum());
 console.log("lee.sum()", lee.sum());
 
-var d1 = new Date('2019-4-10');
-console.log('d1.getFullYear()', d1.getFullYear());
-console.log('d1.getMonth()', d1.getMonth());
