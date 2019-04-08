0ee2e0070b1172e5bf65ba32616db859d7a2d13e
egoing <egoing@gmail.com>
2019-04-08 11:46:15 +0900

    7. prototype

---
+++ prototype.js
@@ -0,0 +1,17 @@
+function Person(name, first, second, third){
+    this.name=name;
+    this.first=first;
+    this.second=second;   
+}
+
+Person.prototype.sum = function(){
+    return 'prototype : '+(this.first+this.second);
+}
+
+var kim = new Person('kim', 10, 20);
+kim.sum = function(){
+    return 'this : '+(this.first+this.second);
+}
+var lee = new Person('lee', 10, 10);
+console.log("kim.sum()", kim.sum());
+console.log("lee.sum()", lee.sum());
