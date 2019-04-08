40b9ea32b4af71f504855d1fb8cd251eeac6df46
egoing <egoing@gmail.com>
2019-04-08 12:43:16 +0900

    8.2. 클래스의 생성

---
+++ class.js
@@ -0,0 +1,13 @@
+class Person{
+
+}
+
+var kim = new Person();
+console.log('kim', kim);
+
+// kim.sum = function(){
+//     return 'this : '+(this.first+this.second);
+// }
+// var lee = new Person('lee', 10, 10);
+// console.log("kim.sum()", kim.sum());
+// console.log("lee.sum()", lee.sum());
