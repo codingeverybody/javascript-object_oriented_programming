f02be6398f7492b45fba3a132086f358b0b0aa0e
egoing <egoing@gmail.com>
2019-04-09 23:15:30 +0900

    16.2. 생성자 상속 - 부모 생성자 실행

---
+++ constructor-inheritance.js
@@ -0,0 +1,20 @@
+function Person(name, first, second){
+    this.name = name;
+    this.first = first;
+    this.second = second;
+}
+Person.prototype.sum = function(){
+    return this.first + this.second;
+}
+
+function PersonPlus(name, first, second, third){
+    Person.call(this, name,first,second);
+    this.third = third;
+}
+
+PersonPlus.prototype.avg = function(){
+    return (this.first+this.second+this.thrid)/3;
+}
+var kim = new PersonPlus('kim', 10, 20, 30);
+console.log("kim.sum()", kim.sum());
+console.log("kim.avg()", kim.avg());
