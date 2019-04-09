37b0622c88e70f85e6c42efa0f8bfac59c8aaeb8
egoing <egoing@gmail.com>
2019-04-09 18:46:54 +0900

    14.2. call

---
+++ object_function.js
@@ -0,0 +1,8 @@
+var kim = {name:'kim', first:10, second:20}
+var lee = {name:'lee', first:10, second:10}
+function sum(prefix){
+    return prefix+(this.first+this.second);
+}
+// sum();
+console.log("sum.call(kim)", sum.call(kim, '=> ')); //apply
+console.log("lee.call(kim)", sum.call(lee, ': '));
