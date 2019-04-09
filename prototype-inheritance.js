8c8f23456b35aa0e25c74780c3e920ad105234f0
egoing <egoing@gmail.com>
2019-04-09 13:07:50 +0900

    13.3. Object.create()

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,6 +1,9 @@
 var superObj = {superVal:'super'}
-var subObj = {subVal:'sub'}
-subObj.__proto__ = superObj;
+// var subObj = {subVal:'sub'}
+// subObj.__proto__ = superObj;
+var subObj = Object.create(superObj);
+subObj.subVal = 'sub';
+debugger;
 console.log('subObj.subVal =>', subObj.subVal);
 console.log('subObj.superVal =>', subObj.superVal);
 subObj.superVal = 'sub';
