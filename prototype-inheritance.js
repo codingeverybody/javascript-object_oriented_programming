cfa8c127b7c305ff40aa28ceb31d5839a1c43c1e
egoing <egoing@gmail.com>
2019-04-09 14:59:33 +0900

    13.4. 객체 상속 활용하기

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -8,3 +8,28 @@ console.log('subObj.subVal =>', subObj.subVal);
 console.log('subObj.superVal =>', subObj.superVal);
 subObj.superVal = 'sub';
 console.log('superObj.superVal =>', superObj.superVal);
+
+var kim = {
+    name:'kim',
+    first:10, second:20,
+    sum:function(){return this.first+this.second}
+}
+
+// var lee = {
+//     name:'lee',
+//     first:10, second:10, 
+//     avg:function(){
+//         return (this.first+this.second)/2;
+//     }
+// }
+// lee.__proto__ = kim;
+
+var lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 10;
+lee.avg = function(){
+    return (this.first+this.second)/2;
+}
+console.log('lee.sum() : ', lee.sum());
+console.log('lee.avg() : ', lee.avg());
